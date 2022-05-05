1404fa6519e4917fc89763fb91a67f6ff5afa0d6
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2022-05-05 21:40:11 +0530

    Updates

---
+++ precache-manifest.d66c426028ae037414320c452331ae8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b8587f38fe53c0b80f6",
+    "url": "/Code_snippet/static/css/main.86963dd5.chunk.css"
+  },
+  {
+    "revision": "1b8587f38fe53c0b80f6",
+    "url": "/Code_snippet/static/js/main.1b8587f3.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/Code_snippet/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "d27e3ffcf6bfe21db692",
+    "url": "/Code_snippet/static/js/2.d27e3ffc.chunk.js"
+  },
+  {
+    "revision": "4c6b6a824078497bcba9",
+    "url": "/Code_snippet/static/js/runtime~main.4c6b6a82.js"
+  },
+  {
+    "revision": "6990321bdd23e7f176de3aebce7c73bc",
+    "url": "/Code_snippet/static/media/record-img.6990321b.png"
+  },
+  {
+    "revision": "e3287a2ba22b00aa7543d54b7d2bd5c2",
+    "url": "/Code_snippet/index.html"
+  }
+];
