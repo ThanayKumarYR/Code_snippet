1daa2714071b252fcc22e9a529dd15d5db26bc3a
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2022-05-14 11:43:26 +0530

    Updates

--- precache-manifest.1133862c99b3ca27f66d1186f229f9e5.js
+++ precache-manifest.1133862c99b3ca27f66d1186f229f9e5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "25eb33a9a689097bba4b",
-    "url": "/Code_snippet/static/css/main.09d2f8be.chunk.css"
+    "revision": "11420ebe7bf4e764a81e",
+    "url": "/Code_snippet/static/css/main.c5824ff8.chunk.css"
   },
   {
-    "revision": "25eb33a9a689097bba4b",
-    "url": "/Code_snippet/static/js/main.25eb33a9.chunk.js"
+    "revision": "11420ebe7bf4e764a81e",
+    "url": "/Code_snippet/static/js/main.11420ebe.chunk.js"
   },
   {
     "revision": "517527aa6758397229f0",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/Code_snippet/static/media/record-img.6990321b.png"
   },
   {
-    "revision": "b18ff1b78f9d8f3daf1a9cf4339ff1bd",
+    "revision": "3000d48aa463d54defc276bfc2b393fe",
     "url": "/Code_snippet/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Code_snippet/precache-manifest.dbc4175ce11b247eb0590b83b9100e26.js"
+  "/Code_snippet/precache-manifest.1133862c99b3ca27f66d1186f229f9e5.js"
 );
 
 workbox.clientsClaim();
